5a3652542a11d6cef4625c2cc993e940b6dc4ec3
ekyarsakarya-eng <eky.arsakarya@gmail.com>
2026-05-09 08:15:01 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,17 @@
-const CACHE_NAME = 'ABSENSI PAMILI GARMEN - V7.1 FINAL';
+const CACHE_NAME = 'ABSENSI-PG-V8';
 const URLS_TO_CACHE = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
+  '/absensi-PG/',
+  '/absensi-PG/index.html',
+  '/absensi-PG/app.js',
+  '/absensi-PG/manifest.json',
+  '/absensi-PG/icon-192.png',
+  '/absensi-PG/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
   self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return Promise.all(
-        URLS_TO_CACHE.map(url => cache.add(url).catch(()=>{}))
-      );
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
   );
 });
 
@@ -28,25 +25,11 @@ self.addEventListener('activate', event => {
 });
 
 self.addEventListener('fetch', event => {
-  const url = event.request.url;
-  // jangan cache Google Apps Script
-  if (url.includes('script.google.com') || url.includes('nominatim.openstreetmap.org')) {
+  if (event.request.url.includes('script.google.com') || 
+      event.request.url.includes('nominatim.openstreetmap.org')) {
     return event.respondWith(fetch(event.request));
   }
   event.respondWith(
-    caches.match(event.request).then(resp => {
-      return resp || fetch(event.request).then(fetchRes => {
-        // cache hanya file lokal
-        if (event.request.method === 'GET' && fetchRes.status === 200 && fetchRes.type === 'basic') {
-          const clone = fetchRes.clone();
-          caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
-        }
-        return fetchRes;
-      });
-    }).catch(() => {
-      if (event.request.mode === 'navigate') {
-        return caches.match('./index.html');
-      }
-    })
+    caches.match(event.request).then(resp => resp || fetch(event.request))
   );
 });
